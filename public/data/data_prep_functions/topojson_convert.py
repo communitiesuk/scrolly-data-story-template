3b9e2a31f9c3209a163d94e89465ea74a712d611
Gordon Donald <gordon.donald@levellingup.gov.uk>
2023-04-18 17:46:03 +0100

    Amend topojson convert to also sniff out code column and chnage it to GeoCode -- this simplifies the svelte code.

--- public/data/data_prep_functions/topojson_convert.py
+++ public/data/data_prep_functions/topojson_convert.py
@@ -9,12 +9,26 @@ import geopandas
 import pandas as pd
 import topojson as tp
 
+#Use Regex to find the column with the ONS area codes. 
+#This means we can change it to 'GeoCode' for all geographies and use the same svelte function to look at boundaries.
+
+def get_code_column(dataset):
+    col = dataset.columns[dataset.stack().str.match('[ESWN][0-9]{8}').groupby(level=1, sort=False).any()]
+    if len(col)==0:
+        print("ERROR: No columns that look like the contain geography codes found. Checking if the flag entered matches the expected pattern")
+        return(0)
+    return(col[0])
+
 #Convert files to topojson format and ensure we using CRS svelte code expects
-def topojson(file_in, file_out):
+def topojson(file_in, file_out, code_col="auto"):
     shape = geopandas.read_file(file_in)
     shape = shape.to_crs('EPSG:4326')
+    if code_col == 'auto':
+        code_col = get_code_column(shape)
+    shape.columns = shape.columns.str.replace(code_col, "GeoCode")
     tp.Topology(shape).to_json(file_out)
     
 
 topojson("LA_centroid.geojson", "LA_centroid.json")
 topojson("tees_lsoas.geojson", 'tess_lsoas.json')
+topojson("Local_Authority_Districts_December_2021_GB_BUC_2022_1023427260691650215.geojson", "LAD.json")
--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,29 @@ export function setColors(themes, theme) {
       return !mediaQuery || mediaQuery.matches ? false : true; // return true for motion, false for no motion
   }
   
+
+  	// Functions for map component
+	export function fitBounds(bounds , map_id) {
+    let animation = getMotion(); // Set animation preference depending on browser preference
+		if (map_id) {
+			map_id.fitBounds(bounds, {animate: animation, padding: 30});
+		}
+	}
+	export function fitById(id, map_id, geo) {
+		if (geo && id) {
+			let feature = geo.features.find(d => d.properties.GeoCode == id);
+			let bounds = bbox(feature.geometry);
+			fitBounds(bounds, map_id);
+		}
+	}
+	export function fitFeatures(codes, map_id, geo) {
+		if (map_id) {
+			if (!Array.isArray(codes)) {
+				codes = [codes];
+			}
+			let features = geo.features.filter(f => codes.includes(f.properties.GeoCode));
+			let polygon = union(...features);
+			let bounds = bbox(polygon);
+			fitBounds(bounds, map_id);
+		}
+	}
